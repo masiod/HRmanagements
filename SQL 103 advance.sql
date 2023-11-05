786e1c1cef67f325ffea94380de66ab9a433f3de
masiod <abda.a.e@hotmail.com>
2023-11-05 22:22:14 +0300

    HRemp

---
+++ SQL 103 advance.sql
@@ -0,0 +1,45 @@
+
+use sales;
+/*
+create table puduct(
+	product_id int not null ,
+    prodcut_name varchar(255) not null,
+    company_id int not null,
+    primary key(product_id),
+    foreign key (company_id) references company(company_id)
+);
+
+create table company(
+	company_id int not null,
+	company_city varchar(255) not null,
+    company_name varchar(255) not null,
+    primary key (company_id)
+);
+insert into company (company_id,company_city,company_name)values
+					(10,'riyadh','Bag Company'),
+                    (11,'jeddah','dress Company'),
+                    (14,'dammam','Shoes Company'),
+                    (15,'riyadh','T-Shirt Company');
+                    
+       
+insert into puduct(product_id,prodcut_name,company_id)values
+					(1,'bag',10),
+                    (3,'t-shirt',15),
+                    (2,'pants',13),
+                    (4,'shorts',13),
+                    (5,'shoes',14);
+                    
+alter table puduct rename to product;  
+                 
+show tables;
+*/
+
+
+
+
+
+                    
+
+                    
+                    
+                    
